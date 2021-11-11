a6c75462df41a9e9505aeb0a2cf9f83206331cbc
giridhar276 <giridhar276@gmail.com>
2021-11-11 19:57:26 +0530

    Add files via upload

--- stringdemo.py
+++ stringdemo.py
@@ -9,6 +9,7 @@ print(aname)
 bname  = """java programming"""
 print(bname)
 # string[start:stop:step]
+# string slicing
 print(name[0])
 print(name[1])
 print(name[0:5])
@@ -22,6 +23,7 @@ print(name[-1])
 print(name[-3:-1])
 print(name[::-1])
 
+# looping
 # range(start,stop,step)
 print("numbers from 1 to 9")
 for val in range(1,10):
@@ -41,11 +43,26 @@ cname = "python"
 for char in cname:
     print(char)
 
-    
-    
 
-    
-    
+
+# methods
+name = "python programming"
+
+print(name.upper())
+print(name.isupper())
+print(name.islower())
+print(name.count('p'))
+print(name.count('z'))
+print(name.replace('python','ruby'))
+print(name.startswith('z'))
+print(name.startswith('p'))
+print(name.find('gra'))
+# will display all the methods available for string name
+print(dir(name))
+
+
+
+
     
 
 
