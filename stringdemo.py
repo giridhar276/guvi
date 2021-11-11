b7274f6bba6c74f397039e5db315d47a570652f3
giridhar276 <giridhar276@gmail.com>
2021-11-11 19:25:45 +0530

    Add files via upload

---
+++ stringdemo.py
@@ -0,0 +1,60 @@
+
+
+name = "python programming"
+print(name)
+
+aname  = 'unix'
+print(aname)
+
+bname  = """java programming"""
+print(bname)
+# string[start:stop:step]
+print(name[0])
+print(name[1])
+print(name[0:5])
+print(name[5:9])
+print(name[::])
+print(name[:])
+print(name[0:17:2])
+print(name[1:17:2])
+print(name[0:16:4])
+print(name[-1])
+print(name[-3:-1])
+print(name[::-1])
+
+# range(start,stop,step)
+print("numbers from 1 to 9")
+for val in range(1,10):
+    print(val)
+    
+print("Even numbers")
+for val in range(2,10,2):
+    print(val)
+
+print(" odd numbers")
+for val in range(1,10,2):
+    print(val)
+
+## iterating string
+print("Iterating string")
+cname = "python"
+for char in cname:
+    print(char)
+
+    
+    
+
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
